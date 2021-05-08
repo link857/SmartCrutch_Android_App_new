7752fab0f82c65cd2339d76164614820a0144acf
DeltaZero <233a344a455@gmail.com>
2021-05-08 22:39:50 +0800

    Init SettingsActivity layout

---
+++ app/src/main/java/deltazero/smartcrutch/ui/SettingsActivity.java
@@ -0,0 +1,16 @@
+package deltazero.smartcrutch.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import deltazero.smartcrutch.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
